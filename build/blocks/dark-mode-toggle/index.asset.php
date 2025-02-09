e934e65616164d230bfb98f8f6b3885298115a7d
Nick Diego <nickmdiego@gmail.com>
2025-02-08 20:23:10 -0600

    Fresh build.

--- build/blocks/dark-mode-toggle/index.asset.php
+++ build/blocks/dark-mode-toggle/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-primitives'), 'version' => '2831d07cfb861ea218f8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-primitives'), 'version' => 'dde02beaf2640ba8048d');
--- build/blocks/notice/index.asset.php
+++ build/blocks/notice/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '034ccf9008cff8b16566');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'e6fd2fec0f5fe4339b24');
